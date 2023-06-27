8cbf27951d809a5356c33b80e63a1650d67e8f9e
Eshnek <dylanpozarnsky@gmail.com>
2023-06-27 15:14:31 -0600

    +

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,10 @@
 import * as vscode from 'vscode';
+import * as backspace from './backspace';
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext): void {
+    activateBackspace(context);
+}
+
+function activateBackspace(context: vscode.ExtensionContext): void {
+    backspace.activate(context);
 }
